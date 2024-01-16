aee34b8a7d1504b25480667fb4b338a7c475d024
Phil Wekullo <philwekullo@gmail.com>
2024-01-16 18:22:41 +0300

    Test API functionality.

--- main.js
+++ main.js
@@ -1,11 +1,39 @@
 
-function testAPI() {
-    const apiUrl = 'https://api.waifu.im/search';
-    const headers = new Headers();
-    headers.append('Accept-Version', 'v5');
-    fetch(apiUrl)
+document.addEventListener('DOMContentLoaded', displayFirstPost)
+
+const apiUrl = 'https://api.waifu.im';
+const headers = new Headers();
+headers.append('Accept-Version', 'v5');
+
+
+function displayFirstPost() {
+    fetch(`${apiUrl}/search`)
     .then(res => res.json())
-    .then(data => {
-        console.log("Data: ", data);
+    .then(images => {
+        let container = document.querySelector('#nsfw-content')
+        container.innerHTML = `
+        <div id="nsfw-img">
+            <img src = ${images.source} alt = '${images.description}'>
+        </div>
+        <div id='img-info'>
+            <p>${images.description}</p>
+            <p id = '2'>Like</p>
+            <p id = '3'>Save</p>            
+        </div>
+        <fieldset>
+            <legend>Select a category:</legend>
+            <div id='sfw-cont'>
+                <input type="radio" id="sfw-radio" name="SFW" value="SFW" checked />
+                <label for="SFW">SFW</label>
+            </div>
+            <div id=''nsfw-cont>
+                <input type="radio" id="nsfw-radio" name="NSFW" value="NSFW" />
+                <label for="NSFW">NSFW</label>
+            </div>
+            <div id='fieldset-btn'>
+                <button type="submit">Submit</button>
+            </div>
+        </fieldset>
+        `
     })
 }
