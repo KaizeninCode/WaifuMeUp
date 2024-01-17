ae264dfcade78f0e2cf3a14e9cf73660853ed420
Phil Wekullo <philwekullo@gmail.com>
2024-01-17 12:46:22 +0300

    Remove fieldset and radio buttons.

--- main.js
+++ main.js
@@ -1,39 +1,45 @@
-
 document.addEventListener('DOMContentLoaded', displayFirstPost)
 
-const apiUrl = 'https://api.waifu.im';
-const headers = new Headers();
-headers.append('Accept-Version', 'v5');
+const apiUrl = 'https://api.waifu.im/search';
+
+async function displayFirstPost() {
+    const response = await fetch(apiUrl)
+    const data = response.json()    
+    let container = document.querySelector('#nsfw-content')
+    container.innerHTML = `
+    <div id="nsfw-img">
+        <img src = ${data.url} alt='${data.image_id}'>
+    </div>
+    <div id='img-info'>
+        <p id = 'a' onclick='likePost()'>Like</p>
+        <p id = 'b' onclick='addToFavs()'>Save</p>            
+    </div>
+    `    
+}
 
 
-function displayFirstPost() {
-    fetch(`${apiUrl}/search`)
-    .then(res => res.json())
-    .then(images => {
-        let container = document.querySelector('#nsfw-content')
-        container.innerHTML = `
-        <div id="nsfw-img">
-            <img src = ${images.source} alt = '${images.description}'>
-        </div>
-        <div id='img-info'>
-            <p>${images.description}</p>
-            <p id = '2'>Like</p>
-            <p id = '3'>Save</p>            
-        </div>
-        <fieldset>
-            <legend>Select a category:</legend>
-            <div id='sfw-cont'>
-                <input type="radio" id="sfw-radio" name="SFW" value="SFW" checked />
-                <label for="SFW">SFW</label>
-            </div>
-            <div id=''nsfw-cont>
-                <input type="radio" id="nsfw-radio" name="NSFW" value="NSFW" />
-                <label for="NSFW">NSFW</label>
-            </div>
-            <div id='fieldset-btn'>
-                <button type="submit">Submit</button>
-            </div>
-        </fieldset>
-        `
+function likePost(){
+    let like = document.getElementById('a')
+    like.addEventListener('click', () => alert('Added to Liked Posts.'))
+}
+
+function addToFavs(){
+    let save = document.getElementById('b')
+    save.addEventListener('click', () => {
+        alert('Added to Favorites.')
+        fetch('https://api.waifu.im/fav/toggle', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({image_id: data.id})
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log('Success:', data);
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
     })
 }
