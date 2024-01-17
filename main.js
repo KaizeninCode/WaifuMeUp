647a452129f0d358273f346dcbfd0d012d36dd13
Phil Wekullo <philwekullo@gmail.com>
2024-01-18 00:15:29 +0300

    Add code for light/dark mode toggle and add new light mode bg image

--- main.js
+++ main.js
@@ -1,38 +1,91 @@
+//DARK-LIGHT MODE TOGGLE
+const themeToggle = document.querySelector('#theme-toggle');
+
+themeToggle.addEventListener('click', () => {
+  document.body.classList.toggle('dark');
+  themeToggle.classList.toggle('dark');
+
+  if (document.body.classList.contains('dark')) {
+    localStorage.setItem('theme', 'dark');
+  } else {
+    localStorage.setItem('theme', 'light');
+  }
+});
+
+const storedTheme = localStorage.getItem('theme');
+
+if (storedTheme === 'dark') {
+  document.body.classList.add('dark');
+  themeToggle.classList.add('dark');
+}
+//DOM FUNCTIONS
 document.addEventListener('DOMContentLoaded', displayFirstPost)
 
-const apiUrl = 'https://api.waifu.im/search';
+// https://api.waifu.im/fav/toggle
+// http://localhost:3000/waifus/20
+// http://localhost:3000/saved-posts
+// http://localhost:3000/favorites
 
-async function displayFirstPost() {
-    const response = await fetch(apiUrl)
-    const data = response.json()    
-    let container = document.querySelector('#nsfw-content')
-    container.innerHTML = `
-    <div id="nsfw-img">
-        <img src = ${data.url} alt='${data.image_id}'>
-    </div>
-    <div id='img-info'>
-        <p id = 'a' onclick='likePost()'>Like</p>
-        <p id = 'b' onclick='addToFavs()'>Save</p>            
-    </div>
-    `    
+
+const apiUrl = 'https://api.waifu.im/search';
+ function displayFirstPost() {
+    fetch(apiUrl)
+    .then(res => res.json())
+    .then(data => {
+        let container = document.querySelector('#nsfw-content')
+        container.innerHTML = `
+        <div id="nsfw-img">
+            <img src = ${data.source} alt=${data.name}>
+        </div>
+        <div id='img-info'>
+            <p id = 'a' onclick='likePost()'>Like</p>
+            <p id = 'b' onclick='addToFavs()'>Save</p>            
+        </div>
+        `    
+    })    
 }
+
+
 
 
 function likePost(){
     let like = document.getElementById('a')
     like.addEventListener('click', () => alert('Added to Liked Posts.'))
+    // like.addEventListener('click', () => {
+    //     alert('Added to Liked Posts')
+    //     fetch('', {
+    //         method: 'POST',
+    //         headers: {
+    //             'Content-Type': 'application/json',
+    //         },
+    //         body: JSON.stringify({
+    //             name: data.name,
+    //             imageUrl: data.imageUrl
+    //         })
+    //     })
+    //     .then(response => response.json())
+    //     .then(data => {
+    //         console.log('Success:', data);
+    //     })
+    //     .catch(error => {
+    //         console.error('Error:', error);
+    //     });
+    // })
 }
 
 function addToFavs(){
     let save = document.getElementById('b')
     save.addEventListener('click', () => {
-        alert('Added to Favorites.')
-        fetch('https://api.waifu.im/fav/toggle', {
+        alert('Added to Favorites')
+        fetch('https://api.waifu.im/fav/insert', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
             },
-            body: JSON.stringify({image_id: data.id})
+            body: JSON.stringify({
+                name: data.name,
+                imageUrl: data.imageUrl
+            })
         })
         .then(response => response.json())
         .then(data => {
